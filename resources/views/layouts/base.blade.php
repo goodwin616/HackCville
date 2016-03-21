5b255445728510ebb5bb070e581d33ac88e5a613
Upal <uks2sh@virginia.edu>
2016-03-21 19:02:11 -0400

    Made the footer its own Blade layout file

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,4 +1,5 @@
 @include('layouts.navbar')
+@include('layouts.footer')
 
 <!DOCTYPE html>
 <html lang="en">
@@ -32,53 +33,7 @@
 </body>
 
 <footer class="omg_component footer-7">
-    <div class="bg-color bg-black color-white" style="background-color: rgb(9, 9, 9);">
-        <div class="container padd-top-xs padd-bottom-lg">
-            <div class="row capitalize xs-text-left">
-                <div class="col-md-5">
-
-                    <p class="color-primary padd-top-xs">Hackcville builds student careers in entrepreneurship and falconry.
-                        <br>
-                        <br>we are a 501(c)3 non-profit organization and we deserve to get some donations from your broke-ass.</p>
-                    <p class="padd-bottom-xs color-primary social">
-                        <a href="#" class="icon ion-social-facebook"></a>
-                        <a href="#" class="icon ion-social-googleplus"></a>
-                        <a href="#" class="icon ion-social-instagram"></a>
-                        <a href="#" class="icon ion-social-twitter"></a>
-                    </p>
-                    <p class="color-primary terms">9 elliewood avenue
-                        <br>Charlottesville, VA 22903
-                        <br>
-                    </p>
-                </div>
-                <div class="col-md-2 col-md-offset-1">
-                    <h4 class="hero-subtitle padd-bottom-xs padd-top-xs">
-                            Categories
-                        </h4>
-                    <p class="color-primary"><a href="#" class="">About</a>
-                    </p>
-                    <p class="color-primary"><a href="#" class="">T</a>eam</p>
-                    <p class="color-primary"><a href="#" class="">E</a>vents</p>
-                    <p class="color-primary">Media&nbsp;</p>
-                    <p class="color-primary"><a href="#" class="">C</a>ontact</p>
-                    <p class="color-primary"><a href="#" class="">A</a>pply</p>
-                </div>
-
-                <div class="col-md-2">
-                    <h4 class="hero-subtitle padd-bottom-xs padd-top-xs">
-                            Quick Links
-                        </h4>
-                    <p class="color-primary">Hustle Class</p>
-                    <p class="color-primary">The pioneer</p>
-                    <p class="color-primary">Rethink</p>
-                </div>
-                <div class="col-md-2">
-                    <h4 class="hero-subtitle padd-bottom-xs padd-top-xs">
-                            hello@hackcville.com</h4>
-                </div>
-            </div>
-        </div>
-    </div>
+    @yield('footer')
 </footer>
 
 </html>
